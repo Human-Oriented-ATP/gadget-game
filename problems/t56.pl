6396a8d94f326fe178f8dfc760163c486c6df237
wtgowers <wtg10@cam.ac.uk>
2025-03-11 14:59:45 +0000

    Add files via upload

--- problems/t56.pl
+++ problems/t56.pl
@@ -16,6 +16,6 @@ o(A,B) :- g(A,B), g(C,B), b(A,C), c(C,B).
 w(A) :- g(A,B), c(A,B), w(B). 
 w(B) :- p(F,A,B), w(A), bl(F). 
 g(C,D) :- g(A,B), p(F,A,C), striped_p(F,B,D).
-b(C,D) :- b(A,B), p(F,A,C), p(F,B,D), striped_bl(F).
+b(C,D) :- b(A,B), p(F,A,C), p(F,B,D), striped_w(F).
 c(C,D) :- g(C,D), w(C), y(D).
 :- o(4,2).

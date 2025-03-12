66487cf0057c66afc1c9f314233c4b56597aef1d
wtgowers <wtg10@cam.ac.uk>
2025-03-12 16:39:37 +0000

    Add files via upload

--- problems/t59.pl
+++ problems/t59.pl
@@ -1,12 +1,15 @@
 r(1,1).
 r(2,4).
 r(3,4).
-r(xr(A),A).
+r(A,xr(A)).
 g(xg(A,B),A,B).
+r(B,1) :- r(B,C), g(C,A,A). 
+w(A,1) :- r(A,1).
 g(1,A,A).
-g(A,B,B) :- r(A,1).
+g(A,B,B) :- r(A,1). 
 w(B,C) :- g(1,B,C).
 g(1,B,C) :- w(B,C).
+g(1,A,B) :- g(C,A,B), w(C,1). 
 g(C,A,B) :- r(F,C), r(D,A), r(E,B), g(F,D,E).
 :- w(2,3).
 

02fb2e8bf368cfed3bcdee57dbe3e066b7b35a1f
wtgowers <wtg10@cam.ac.uk>
2025-03-13 17:15:39 +0000

    Add files via upload

--- problems/t61.pl
+++ problems/t61.pl
@@ -2,8 +2,8 @@ b(3,1,2).
 b(6,4,5).
 g(7,3,6).
 g(xg(A,B),A,B).
-g(C,A,B) :- g(A,D,F), g(B,E,F), g(G,D,E), b(C,G,F).
-b(C,G,F) :- g(A,D,F), g(B,E,F), g(G,D,E), g(C,A,B).
+g(C,A,B) :- b(A,D,F), b(B,E,F), g(G,D,E), b(C,G,F).
+b(C,G,F) :- b(A,D,F), b(B,E,F), g(G,D,E), g(C,A,B).
 o(xo(A,B),A,B).
 o(A,B,C) :- o(A,C,B).
 b(C,A,B) :- o(A,D,F), o(B,E,F), b(C,D,E).

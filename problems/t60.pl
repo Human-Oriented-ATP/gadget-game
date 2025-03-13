0d6a44449f4c980d9bf2373785d844db2c26bbba
wtgowers <wtg10@cam.ac.uk>
2025-03-13 13:48:00 +0000

    Add files via upload

--- problems/t60.pl
+++ problems/t60.pl
@@ -5,6 +5,8 @@ r(6,3).
 g(7,4,5).
 b(A,B) :- r(B,A).
 y(xy(A),A).
+g(xg(A,B),A,B).
+r(C,B) :- r(C,A), y(B,A).
 y(A,B) :- y(B,A). 
 b(C,B) :- b(A,B), b(D,B), y(D,A), r(C,A).
 b(C,F) :- g(C,A,B), g(F,D,E), b(A,D), b(B,E). 

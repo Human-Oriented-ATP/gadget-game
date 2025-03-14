d6a3dc6a1f1344cf83096c840560db398c51cc2d
wtgowers <wtg10@cam.ac.uk>
2025-03-14 09:35:03 +0000

    Add files via upload

---
+++ problems/t63.pl
@@ -0,0 +1,18 @@
+g(1,2,3).
+g(2,3,1).
+g(3,1,2).
+r(4,5).
+g(5,1,1).
+r(xr(A),A).
+r(A,B) :- r(B,A).
+g(A,4,A).
+g(A,A,4).
+g(C,A,B) :- r(C,E), r(A,D), g(E,D,B).
+g(C,A,B) :- r(C,E), r(B,D), g(E,A,D).
+g(E,A,F) :- g(D,A,B), g(E,D,C), g(F,B,C).
+g(E,D,C) :- g(D,A,B), g(E,A,F), g(F,B,C).
+r(A,B) :- g(A,5,B).
+g(A,5,B) :- r(A,B).
+r(A,B) :- g(A,B,5).
+g(A,B,5) :- r(A,B). 
+:- g(5,2,2).

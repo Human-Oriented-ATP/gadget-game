e5de4ed8b51e34d58e026f8283be0a8b6b05b831
wtgowers <wtg10@cam.ac.uk>
2025-03-14 18:23:19 +0000

    Add files via upload

--- problems/t64.pl
+++ problems/t64.pl
@@ -5,6 +5,9 @@ g(4,1,1).
 g(5,3,2).
 g(6,5,1).
 r(xr(A),A).
+r(A,B) :- r(B,A).
+g(A,4,B) :- g(A,B,4).
+g(A,B,4) :- g(A,4,B).
 g(xg(A,B),A,B).
 g(E,A,F) :- g(D,A,B), g(E,D,C), g(F,B,C).
 g(E,D,C) :- g(D,A,B), g(E,A,F), g(F,B,C).

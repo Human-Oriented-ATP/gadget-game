c549f53028a00bd5003250307b9ad5f7f4bf585b
wtgowers <wtg10@cam.ac.uk>
2025-03-19 12:16:19 +0000

    Add files via upload

--- problems/t68.pl
+++ problems/t68.pl
@@ -4,6 +4,7 @@ b(4,2).
 g(xg(A,B),A,B).
 r(A,C) :- r(A,B), r(B,C).
 b(A,B) :- g(A,B,B).
+g(A,B,B) :- b(A,B).
 r(D,E) :- g(D,A,C), g(E,B,C), r(A,B).
 r(D,E) :- g(D,C,A), g(E,C,B), r(A,B).
 :- r(3,4).

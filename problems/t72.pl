b79e93873b21fac2c7db37f67b60644c36b789e8
wtgowers <wtg10@cam.ac.uk>
2025-03-20 22:33:37 +0000

    Add files via upload

--- problems/t72.pl
+++ problems/t72.pl
@@ -1,14 +1,6 @@
-g(xg(A),A).
-b(xb(A),A).
-o(xo(A),A).
-p(xp(A),A).
-r(A,B) :- g(C,A), g(B,D).
-g(A,B) :- b(C,A).
-g(A,B) :- b(B,C).
-b(A,B) :- o(C,A).
-b(A,B) :- o(B,C).
-o(A,B) :- p(C,A,D).
-o(A,B) :- p(B,C,D).
-p(A,B,C) :- w(A,C).
-w(A,B) :- w(B,A).
-:- r(1,2).
+g(1,8,9).
+g(2,7,8).
+g(3,7,9).
+g(xg(A,B),A,B).
+g(A,B,C) :- g(A,D,E), g(B,D,F), g(C,E,F).
+:- g(1,2,3).

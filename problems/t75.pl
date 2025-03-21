a6f6d0fba3a62aca715f9d22922c134597ca4261
wtgowers <wtg10@cam.ac.uk>
2025-03-21 19:41:24 +0000

    Add files via upload

--- problems/t75.pl
+++ problems/t75.pl
@@ -2,6 +2,6 @@ r(1,2).
 r(1,3).
 r(2,3).
 g(xg(A,B),A,B).
-g(A,B,C) :- r(A,B), r(A,C), r(B,C).
-r(A,B) :- g(C,D,A), g(F,E,B), r(D,E).
+g(A,B,C) :- g(A,B,D), g(A,C,D), g(B,C,D).
+g(A,B,G) :- g(C,D,A), g(F,E,B), r(D,E,G).
 :- g(4,5,6).

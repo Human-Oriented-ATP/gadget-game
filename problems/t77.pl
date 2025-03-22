1ab652358a5f3a03144a0ac01c23057a4d72de51
wtgowers <wtg10@cam.ac.uk>
2025-03-22 10:24:57 +0000

    Add files via upload

---
+++ problems/t77.pl
@@ -0,0 +1,11 @@
+r(A,C) :- g(A,B), g(B,C).
+g(A,C) :- b(A,B), b(B,C).
+b(A,C) :- o(A,B), o(B,C).
+o(A,C) :- b(A,B), b(B,C).
+o(A,C) :- p(A,B), p(B,C).
+p(A,C) :- o(A,B), o(B,C).
+bl(A,C) :- p(A,B), p(B,C).
+p(A,B) :- c(A,C,B,D).
+p(A,B) :- c(C,B,D,A).
+c(A,B,C,D) :- bl(C,D).
+:- r(1,2).

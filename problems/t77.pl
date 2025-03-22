bc56a47ed53f30abc4fd01785a33406b9ec0a059
wtgowers <wtg10@cam.ac.uk>
2025-03-22 10:36:18 +0000

    Add files via upload

--- problems/t77.pl
+++ problems/t77.pl
@@ -1,4 +1,12 @@
-r(A,C) :- g(A,B), g(B,C).
+
+w(3).
+b(xb(A),A).
+b(A,xb(A)).
+o(xo(A),A).
+o(A,xo(A)).
+p(xp(A),A).
+p(A,xp(A)).
+r(A,C) :- g(A,B), w(B), g(B,C).
 g(A,C) :- b(A,B), b(B,C).
 b(A,C) :- o(A,B), o(B,C).
 o(A,C) :- b(A,B), b(B,C).
@@ -9,3 +17,4 @@ p(A,B) :- c(A,C,B,D).
 p(A,B) :- c(C,B,D,A).
 c(A,B,C,D) :- bl(C,D).
 :- r(1,2).
+

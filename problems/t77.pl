e49a5f6e6302e995720aef024a1dcc2cffcd85c5
wtgowers <wtg10@cam.ac.uk>
2025-03-22 11:19:45 +0000

    Add files via upload

--- problems/t77.pl
+++ problems/t77.pl
@@ -1,14 +1,14 @@
 
 w(3).
 b(xb(A),A).
-striped_b(A,xb(A)).
+striped_b(xb(A),A).
 o(xo(A),A).
-striped_o(A,xo(A)).
+striped_o(xo(A),A).
 p(xp(A),A).
-striped_p(A,xp(A)).
-b(A,B) :- striped_b(A,B).
-o(A,B) :- striped_o(A,B).
-p(A,B) :- striped_p(A,B).
+striped_p(xp(A),A).
+b(A,B) :- striped_b(B,A).
+o(A,B) :- striped_o(B,A).
+p(A,B) :- striped_p(B,A).
 r(A,C) :- g(A,B), w(B), g(B,C).
 g(A,C) :- b(A,B), b(B,C).
 b(A,C) :- o(A,B), o(B,C).

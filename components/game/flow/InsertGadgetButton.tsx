6767df7991aca81be8f510780038996c70cb09cc
Jonas Bayer <jonas.bayer999@gmail.com>
2025-07-23 10:01:04 +0200

    Fix: Empty object lint

--- components/game/flow/InsertGadgetButton.tsx
+++ components/game/flow/InsertGadgetButton.tsx
@@ -1,28 +1,25 @@
-import { Axiom } from "lib/game/Primitives";
 import { useGameStateContext } from "lib/state/StateContextProvider";
 import { useRef } from "react";
 import { useShallow } from "zustand/react/shallow";
 
-interface InsertGadgetButtonProps extends React.PropsWithChildren<{}> {
-    axiom: string;
-}
+type InsertGadgetButtonProps = React.PropsWithChildren<{ axiom: string }>;
 
 export function InsertGadgetButton({ axiom, children }: InsertGadgetButtonProps): React.JSX.Element {
-    const addGadgetNode = useGameStateContext(useShallow((state) => state.addGadgetNode));
-    const abortAddingGadget = useGameStateContext(useShallow((state) => state.abortAddingGadget));
+  const addGadgetNode = useGameStateContext(useShallow((state) => state.addGadgetNode));
+  const abortAddingGadget = useGameStateContext(useShallow((state) => state.abortAddingGadget));
 
-    const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
-    function getPosition() {
-        const domRect = (ref.current as HTMLElement).getBoundingClientRect();
-        return { x: domRect.left + domRect.width / 2, y: domRect.top + domRect.height / 2 };
-    }
+  function getPosition() {
+    const domRect = (ref.current as HTMLElement).getBoundingClientRect();
+    return { x: domRect.left + domRect.width / 2, y: domRect.top + domRect.height / 2 };
+  }
 
-    function onMouseDown(e: React.MouseEvent) {
-        addGadgetNode(axiom, getPosition());
-    }
+  function onMouseDown(e: React.MouseEvent) {
+    addGadgetNode(axiom, getPosition());
+  }
 
-    return <div ref={ref} className="flex justify-center px-1" onMouseDown={onMouseDown} onClick={abortAddingGadget}>
-        {children}
-    </div>;
+  return <div ref={ref} className="flex justify-center px-1" onMouseDown={onMouseDown} onClick={abortAddingGadget}>
+    {children}
+  </div>;
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -53,7 +53,6 @@ export default tseslint.config(
   // Later rules take precedence.
   {
     "rules": {
-      "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-extra-non-null-assertion": "off",
       "@typescript-eslint/no-floating-promises": "off",

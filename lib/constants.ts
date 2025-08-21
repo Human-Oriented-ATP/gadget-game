a15b78bd073e8c195901cda520b3f2aa2b69606d
Jonas Bayer <jonas.bayer999@gmail.com>
2025-08-21 07:07:46 +0200

    study: increase length to 45 minutes

---
+++ lib/constants.ts
@@ -0,0 +1,1 @@
+export const STUDY_DURATION_IN_S = 45 * 60;
--- lib/study/submitQuestionnaire.ts
+++ lib/study/submitQuestionnaire.ts
@@ -5,24 +5,24 @@ import { getPlayerId } from "./synchronizeHistory";
 import { StudyConfiguration } from "./Types";
 import { cookies } from "next/headers";
 import { getProblemList } from "./LevelConfiguration";
+import { STUDY_DURATION_IN_S } from "lib/constants";
 
-const MAXIMUM_STUDY_DURATION = 45 * 60 * 1000;
 const COMPLETION_THRESHOLD = 0.9;
 
 export async function submitQuestionnaire1(formData: string) {
-    const playerId = await getPlayerId();
-    const questionnaire = JSON.parse(formData);
-    console.log(questionnaire);
-    await sql`INSERT INTO questionnaire_responses (respondent_id, education_level, math_training, math_regularity, 
+  const playerId = await getPlayerId();
+  const questionnaire = JSON.parse(formData);
+  console.log(questionnaire);
+  await sql`INSERT INTO questionnaire_responses (respondent_id, education_level, math_training, math_regularity, 
         specialty, first_language, prolific_id, feedback)
         VALUES (${playerId}, ${questionnaire.educationLevel}, ${questionnaire.mathTraining}, ${questionnaire.mathRegularity}, 
         ${questionnaire.specialty}, ${questionnaire.firstLanguage}, ${questionnaire.prolific}, ${questionnaire.feedback})`;
 }
 
 export async function submitQuestionnaire2(formData: string) {
-    const playerId = await getPlayerId();
-    const questionnaire = JSON.parse(formData);
-    await sql`UPDATE questionnaire_responses
+  const playerId = await getPlayerId();
+  const questionnaire = JSON.parse(formData);
+  await sql`UPDATE questionnaire_responses
     SET 
         difficulty = ${questionnaire.difficulty}, 
         enjoyableness = ${questionnaire.enjoyableness}, 
@@ -34,35 +34,35 @@ export async function submitQuestionnaire2(formData: string) {
 }
 
 export async function hasSubmittedQuestionnaire2() {
-    const submitted = (await cookies()).get("questionnaire2Submitted");
-    return submitted !== undefined;
+  const submitted = (await cookies()).get("questionnaire2Submitted");
+  return submitted !== undefined;
 }
 
 export async function hasCompletedEnoughProblems(config: StudyConfiguration) {
-    const problems = getProblemList(config);
-    const completed = (await cookies()).get("completed");
-    if (completed === undefined) {
-        return false
-    } else {
-        const completedProblems = completed.value.split(",");
-        const ratioSolved = completedProblems.length / problems.length;
-        return ratioSolved >= COMPLETION_THRESHOLD;
-    }
+  const problems = getProblemList(config);
+  const completed = (await cookies()).get("completed");
+  if (completed === undefined) {
+    return false
+  } else {
+    const completedProblems = completed.value.split(",");
+    const ratioSolved = completedProblems.length / problems.length;
+    return ratioSolved >= COMPLETION_THRESHOLD;
+  }
 }
 
 export async function timeIsOver() {
-    const startTime = (await cookies()).get("start_time");
-    if (!startTime) {
-        return false
-    } else {
-        const startDate = new Date(startTime.value)
-        return startDate.valueOf() + MAXIMUM_STUDY_DURATION < Date.now().valueOf();
-    }
+  const startTime = (await cookies()).get("start_time");
+  if (!startTime) {
+    return false
+  } else {
+    const startDate = new Date(startTime.value)
+    return startDate.valueOf() + STUDY_DURATION_IN_S * 1000 < Date.now().valueOf();
+  }
 }
 
 export async function progressSufficientForQuestionnaire2(config) {
-    const completed = await hasCompletedEnoughProblems(config)
-    const timeOver = await timeIsOver();
-    const hasAlreadySubmitted = await hasSubmittedQuestionnaire2();
-    return !hasAlreadySubmitted && (timeOver || completed);
+  const completed = await hasCompletedEnoughProblems(config)
+  const timeOver = await timeIsOver();
+  const hasAlreadySubmitted = await hasSubmittedQuestionnaire2();
+  return !hasAlreadySubmitted && (timeOver || completed);
 }

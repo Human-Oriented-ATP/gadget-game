b443a54f73a9dce86739d3a6f756912a9ab6a5d8
TrueBoxGuy <17554188+TrueBoxGuy@users.noreply.github.com>
2025-10-16 10:07:15 +0100

    Introduce identifiers for function terms
    Rename `label` to `identifier` and introduce
    identifiers to function terms. Remove behaviour
    that allows identifiers be used in assignments.
    Ensure all identifiers are stripped in
    `replaceWithRepresentatives`.

--- lib/game/Term.ts
+++ lib/game/Term.ts
@@ -1,9 +1,11 @@
+import { LapTimerIcon } from "@radix-ui/react-icons"
 import { DisjointSetWithAssignment } from "../util/DisjointSetWithAssignment"
 import { Axiom } from "./Primitives"
 
 export type VariableName = string
 export type FunctionName = string
 export type LabelName = string
+export type IdentifierName = string
 
 // Within problem files, the function terms correspond to
 // skolemization. The arguments will always consist of the
@@ -11,30 +13,31 @@ export type LabelName = string
 // always be uniquely be determined by all three of the
 // term label, function position, and position of other functions
 // in the term.
-export type Term
-  = { number: string, label?: LabelName } | { variable: VariableName }
-  | { function: FunctionName, args: Term[] }
+export type Term 
+    = {number: string, identifier?: IdentifierName } | { variable: VariableName } 
+    | { function: FunctionName, args: Term[], identifier?: IdentifierName }
 export type Relation = { label: LabelName, args: Term[] }
 
 export function occursInNaive(v: string, term: Term): boolean {
-  if ("variable" in term) {
-    return v === term.variable
-  } else if ("number" in term) {
-    return v === term.label
-  } else {
-    const appearsInArg = term.args.map(arg => occursInNaive(v, arg))
-    return appearsInArg.includes(true)
-  }
+    if ("variable" in term) {
+        return v === term.variable
+    } else if ("args" in term) {
+        const appearsInArg = term.args.map(arg => occursInNaive(v, arg))
+        return appearsInArg.includes(true)
+    } else {return false};
 }
 
 export function replaceWithRepresentatives(t: Term, assignment: Assignment): Term {
-  if ("variable" in t) {
-    return { variable: assignment.findRepresentative(t.variable) }
-  } else if ("number" in t) {
-    return { number: t.number }
-  } else {
-    return { function: t.function, args: t.args.map(arg => replaceWithRepresentatives(arg, assignment)) }
-  }
+    if ("variable" in t) {
+        return { variable: assignment.findRepresentative(t.variable) }
+    } else if ("number" in t) {
+        return { number: t.number }
+    } else {
+        return {
+            function: t.function,
+            args: t.args.map(arg => replaceWithRepresentatives(arg, assignment)) 
+        }
+    }
 }
 
 export function occursIn(v: VariableName, term: Term, assignment: Assignment): boolean {
@@ -62,35 +65,34 @@ export function getVariableSet(r: Relation): Set<VariableName> {
 }
 
 export function assignTermDeeply(t: Term, assignment: Assignment): Term {
-  if ("function" in t) {
-    const argsAssigned: Term[] = t.args.map(arg => assignTermDeeply(arg, assignment));
-    return { function: t.function, args: argsAssigned };
-  } else {
-    const label = "variable" in t ? t.variable : t.label;
-    if (label === undefined) return t;
-    const assignedValue = assignment.getAssignedValue(label);
-    if (!assignedValue) return t;
+    if ("function" in t) {
+        const argsAssigned: Term[] = t.args.map(arg => assignTermDeeply(arg, assignment));
+        return { function: t.function, args: argsAssigned };
+    } else if ("variable" in t) {
+        const assignedValue = assignment.getAssignedValue(t.variable);
+        if (!assignedValue) return t;
 
-    if (occursIn(label, assignedValue, assignment))
-      throw Error("Attempting to deeply assign the variables in a term in a cyclic way")
+        if (occursIn(t.variable, assignedValue, assignment)) 
+                throw Error("Attempting to deeply assign the variables in a term in a cyclic way")
 
-    return assignTermDeeply(assignedValue, assignment);
-  }
+        return assignTermDeeply(assignedValue, assignment);
+    } else {return t};
 }
 
 export function makeTermWithFreshLabels(t: Term, prefix: string, number_prefix: string): Term {
-  if ("variable" in t) {
-    return { variable: prefix + "_" + t.variable }
-  } else if ("number" in t) {
-    return { number: t.number, label: number_prefix + "_" + prefix }
-  } else {
-    return {
-      function: t.function,
-      args: t.args.map((arg, p) =>
-        makeTermWithFreshLabels(arg, prefix, `${number_prefix},${p}`)
-      )
+    if ("variable" in t) {
+        return { variable: prefix + "_" + t.variable}
+    } else if ("number" in t) {
+        return {number: t.number, identifier: number_prefix + "_" + prefix}
+    } else {
+        return {
+            function: t.function, 
+            identifier: number_prefix + "_" + prefix,
+            args: t.args.map((arg, p) =>
+                makeTermWithFreshLabels(arg, prefix, `${number_prefix},${p}`)
+            ) 
+        }
     }
-  }
 }
 
 export function makeRelationWithFreshLabels(r: Relation, prefix: string, number_prefix: string): Relation {
@@ -102,8 +104,8 @@ export function makeRelationWithFreshLabels(r: Relation, prefix: string, number_
   }
 }
 
-// Have variable names be unique across axioms, and assign every
-// number term a unique label (so we can track the exact numeric 
+// Have variable names be unique across axioms, and also assign every
+// number and identifier term an identifier (so we can track the exact
 // hole a term is associated with). The prefix should begin with g_, 
 // to not collide with number labels. 
 export function makeAxiomWithFreshLabels(axiom: Axiom, prefix: string): Axiom {

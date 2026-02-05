6b3bc7bbf759c12fcb8abed881d623081995aa0b
Jonas Bayer <jonas.bayer999@gmail.com>
2026-02-05 09:36:54 +0000

    feat: always synchronize history, even when log is empty

--- lib/study/synchronizeHistory.ts
+++ lib/study/synchronizeHistory.ts
@@ -32,29 +32,22 @@ export async function getPlayerId(): Promise<string> {
     }
 }
 
-function passesSanityCheck(history: GameHistory): boolean {
-    const logIsNotEmpty = history.log.length !== 0
-    return logIsNotEmpty
-}
-
 export async function synchronizeHistory(historyString: string) {
     "use server"
     try {
         const playerId = await getPlayerId()
         const history: GameHistory = JSON.parse(historyString)
-        if (passesSanityCheck(history)) {
-            const log: string = JSON.stringify(history.log);
-            const lastSynchronized = new Date().toISOString();
-            const startTime = history.startTime.toString();
-            await sql`INSERT INTO study_data (player_id, problem_id, config, start, latest, history, completed) VALUES 
-                (${playerId}, ${history.problemId}, ${history.configId}, ${startTime}, ${lastSynchronized}, ${log}, ${history.completed})
-                ON CONFLICT (player_id, problem_id, start) DO UPDATE
-                SET latest=${lastSynchronized}, 
-                    history=${log},
-                    completed=${history.completed}
-                WHERE study_data.completed=false;`
-            console.log(`Synchronized history. Player ID: ${playerId}, Problem ID: ${history.problemId}, Config ID: ${history.configId}, Completed: ${history.completed}`)
-        }
+        const log: string = JSON.stringify(history.log);
+        const lastSynchronized = new Date().toISOString();
+        const startTime = history.startTime.toString();
+        await sql`INSERT INTO study_data (player_id, problem_id, config, start, latest, history, completed) VALUES 
+            (${playerId}, ${history.problemId}, ${history.configId}, ${startTime}, ${lastSynchronized}, ${log}, ${history.completed})
+            ON CONFLICT (player_id, problem_id, start) DO UPDATE
+            SET latest=${lastSynchronized}, 
+                history=${log},
+                completed=${history.completed}
+            WHERE study_data.completed=false;`
+        console.log(`Synchronized history. Player ID: ${playerId}, Problem ID: ${history.problemId}, Config ID: ${history.configId}, Completed: ${history.completed}`)
     } catch (error) {
         console.error("Error synchronizing history.")
         console.error(error);

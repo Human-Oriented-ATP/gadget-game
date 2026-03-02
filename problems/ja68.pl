3badc36105e5aad254de7167fd18fba3b5a9ee92
JacobLoader <50699523+JacobLoader@users.noreply.github.com>
2026-03-02 15:58:41 +0000

    fix

--- problems/ja68.pl
+++ problems/ja68.pl
@@ -11,13 +11,14 @@ g(10,2,7).
 g(11,3,7).
 y(3,A).
 
-y(A,B) :- g(A,C,D), g(B,E,F), y(C,E), y(C,F). 
+
 
 b(A,B) :- r(A,B), r(B,C).
 r(A,C) :- r(A,B), r(B,C).
-r(A,B) :- g(A,C,D), g(B,E,F), r(C,E), r(D,F).
+g(A,B,C) :- g(A,C,B)
+r(A,B) :- g(A,C,D), g(B,E,D), r(C,E).
 
-
+y(A,B) :- g(A,C,D), g(B,E,F), y(C,E), y(C,F). 
 w(A,B) :- y(11,A), y(11,B), b(A,B).
 
 :- w(A,B).

2c5b8fe90b1e9d81ec5dea888f37be66b3dc6a05
Jonas Bayer <jonas.bayer999@gmail.com>
2026-03-17 10:34:59 +0000

    fix: make game page dynamix

--- app/[config]/game/[problem_id]/page.tsx
+++ app/[config]/game/[problem_id]/page.tsx
@@ -1,20 +1,6 @@
-import { loadAllStudyConfigurations, loadStudyConfiguration } from "lib/game/LoadProblems";
-import { ProblemCategory } from "lib/study/Types";
 import { GameLoader } from "components/game/GameLoader";
 
-async function generateConfigurationStaticParams(config: string) {
-    const loadedConfig = await loadStudyConfiguration(config)
-    const configCategories = loadedConfig.categories;
-    const generateProblemsParams = (category: ProblemCategory) => 
-        category.problems.map(problem_id => ({config, problem_id}));
-
-    return configCategories.flatMap(generateProblemsParams);
-}
-
-export async function generateStaticParams() {
-    const configurations = await loadAllStudyConfigurations();
-    return configurations.flatMap(generateConfigurationStaticParams);
-}
+export const dynamic = "force-dynamic";
 
 type Params = Promise<{ config: string, problem_id: string }>
 

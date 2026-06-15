08353035d5090a75d5bb15fd3676d2bc49c127d5
TrueBoxGuy <17554188+TrueBoxGuy@users.noreply.github.com>
2026-06-15 19:54:53 +0100

    Update edge layering and selection transparency
    Move edges behind cells to declutter connections,
    and only elevate edges attached to selected
    gadgets. Additionally, make backgrounds of
    selected gadgets translucent so connections to
    underlying occluded gadgets seem more physical.

--- lib/state/slices/Nodes.ts
+++ lib/state/slices/Nodes.ts
@@ -4,9 +4,6 @@ import { CreateStateWithInitialValue } from '../Types';
 import { gadgetDndFromShelfSlice, GadgetDndFromShelfSlice } from './DragGadgetFromShelf';
 import { ConnectorStatus } from 'components/game/gadget/handles/Connector';
 
-export const DEFAULT_NODE_Z_INDEX = undefined;
-export const CONNECTED_NODE_Z_INDEX = 1005;
-
 export type NodeStateInitializedFromData = {
   nodes: GadgetNode[],
 }
